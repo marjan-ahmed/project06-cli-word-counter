e9caef63f9eb677cd0974a1b0c8b1931451e6d19
Marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-04-17 19:00:09 +0500

    created cli word counter.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # project06-cli-word-counter
-This is cli based word counter with some short and interesting coding.
+This is cli based word counter with inquirer.
---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+//This line a shebang.
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bgRed.underline.whiteBright("-----------*********----------Welcome To The Word Counter Program ( Made By Marjan Ahmed)-----------*********----------"));
+console.log("\n");
+
+const answer : {
+    Sentence: string
+
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter a sentence: "
+    }
+]);
+console.log("\t");
+
+//it will seperate the word in an array
+
+let words = answer.Sentence.trim().split(" ");
+console.log(words);
+
+//print the number of sentence
+
+console.log(`You sentence word count is "${words.length}"`);
